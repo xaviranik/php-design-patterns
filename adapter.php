ac7abc68b1cc0e2c2319f230f5a35315ae08d127
xavir <xaviranik.me@gmail.com>
2020-05-20 12:17:27 +0600

    Added: Adapter pattern

--- adapter.php
+++ adapter.php
@@ -1,23 +1,86 @@
 <?php
 
-class Book {
+// Interface for Book
+interface BookInterface {
+	public function open();
+	public function turnPage();
+}
+
+// Interface for eBook Reader
+interface eReaderInterface {
+	public function turnOn();
+	public function pressNextButton();
+}
+
+class Book implements BookInterface {
 
 	public function open()
 	{
-		echo "Opening the book";
+		var_dump("Opening the book");
 	}
 
 	public function turnPage()
 	{
-		echo "Turning the page";
+		var_dump("Turning the page");
+	}
+}
+
+class Kindle implements eReaderInterface {
+
+	public function turnOn()
+	{
+		var_dump("Turning on Kindle");
+	}
+
+	public function pressNextButton()
+	{
+		var_dump("Press the next button on Kindle");
+	}
+}
+
+class Nook implements eReaderInterface {
+
+	public function turnOn()
+	{
+		var_dump("Turning on Nook");
+	}
+
+	public function pressNextButton()
+	{
+		var_dump("Press the next button on Nook");
+	}
+}
+
+// Let's Create a adapter to bind KindleInterface to BookInterface
+class eReaderAdapter implements BookInterface {
+
+	protected $reader;
+
+	public function __construct(eReaderInterface $reader)
+	{
+		$this->reader = $reader;
+	}
+
+	public function open()
+	{
+		return $this->reader->turnOn();
+	}
+
+	public function turnPage()
+	{
+		return $this->reader->pressNextButton();
 	}
 }
 
 class Person {
 	
-	public function read(Book $book)
+	public function read(BookInterface $book)
 	{
 		$book->open();
 		$book->turnPage();
 	}
 }
+
+(new Person())->read(new Book());
+(new Person())->read(new eReaderAdapter(new Kindle));
+(new Person())->read(new eReaderAdapter(new Nook));
